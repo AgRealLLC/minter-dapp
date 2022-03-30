fc7270c906a706141b6edb7ad555b43bb8d0bf3e
AgRealLLC <agrealllc@gmail.com>
2022-03-30 01:22:57 -0600

    updated files

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "YOUR CONTRACT ADDRESS";"0x75d5147E5a7A591d8808B6A44363ecA5ce0f59cb"
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
